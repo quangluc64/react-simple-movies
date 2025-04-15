dbb275daf27a92ca5b4da06d6236bf04ee4b1001
quangluc64 <taquangluc10b3@gmail.com>
2025-04-15 16:15:51 +0700

    Code giao diện movie card

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Fragment } from "react";
 export default function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10">
+      <header className="header flex items-center justify-center gap-x-5 text-white font-medium py-10 mb-5">
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+      {/*  ==== Banner ==== */}
+      <section className="banner h-[500px] page-container mb-20">
         <div className="w-full h-full rounded-xl bg-white relative">
           <div className="overlay absolute inset-0 rounded-xl bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)]"></div>
           <img
@@ -18,11 +19,76 @@ export default function App() {
           <div className="absolute left-5 bottom-5 text-white">
             <h2 className="text-3xl font-bold mb-5">Avengers: Endgame</h2>
             <div className="flex items-center gap-x-3 mb-8">
-              <span className="px-4 py-2 border border-white rounded-md">Action</span>
-              <span className="px-4 py-2 border border-white rounded-md">Adventure</span>
-              <span className="px-4 py-2 border border-white rounded-md">Drama</span>
+              <span className="px-4 py-2 border border-white rounded-md">
+                Action
+              </span>
+              <span className="px-4 py-2 border border-white rounded-md">
+                Adventure
+              </span>
+              <span className="px-4 py-2 border border-white rounded-md">
+                Drama
+              </span>
             </div>
-            <button className="px-6 py-3 bg-primary rounded-lg font-medium">Watch Now</button>
+            <button className="px-6 py-3 bg-primary rounded-lg font-medium">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
+      {/*  ==== Now Playing ==== */}
+      <section className="movies-layout page-container mb-20">
+        <h2 className="text-white text-2xl font-bold mb-10">Now Playing</h2>
+        <div className="movie-list grid grid-cols-3 gap-10">
+          <div className="movie-card p-3 bg-slate-800 rounded-2xl text-white">
+            <img
+              src="https://lumiere-a.akamaihd.net/v1/images/102_gza1510_comp_v002_c5fae827.jpeg?region=0%2C0%2C3840%2C2160"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-xl mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-5">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full px-6 py-3 bg-primary rounded-lg font-bold text-xl">Watch Now</button>
+          </div>
+        </div>
+      </section>
+      {/*  ==== Top Rated ==== */}
+      <section className="movies-layout page-container mb-20">
+        <h2 className="text-white text-2xl font-bold mb-10">Top Rated</h2>
+        <div className="movie-list grid grid-cols-3 gap-10">
+          <div className="movie-card p-3 bg-slate-800 rounded-2xl text-white">
+            <img
+              src="https://lumiere-a.akamaihd.net/v1/images/102_gza1510_comp_v002_c5fae827.jpeg?region=0%2C0%2C3840%2C2160"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-xl mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-5">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full px-6 py-3 bg-primary rounded-lg font-bold text-xl">Watch Now</button>
+          </div>
+        </div>
+      </section>
+      {/*  ==== Trending ==== */}
+      <section className="movies-layout page-container mb-20">
+        <h2 className="text-white text-2xl font-bold mb-10">Trending</h2>
+        <div className="movie-list grid grid-cols-3 gap-10">
+          <div className="movie-card p-3 bg-slate-800 rounded-2xl text-white">
+            <img
+              src="https://lumiere-a.akamaihd.net/v1/images/102_gza1510_comp_v002_c5fae827.jpeg?region=0%2C0%2C3840%2C2160"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-xl mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-5">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full px-6 py-3 bg-primary rounded-lg font-bold text-xl">Watch Now</button>
           </div>
         </div>
       </section>

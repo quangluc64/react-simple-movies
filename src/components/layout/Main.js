27b95a5c8c8eb387ac57390363a2715165a45c86
quangluc64 <taquangluc10b3@gmail.com>
2025-04-19 12:46:44 +0700

    Fetching dữ liệu trang danh sách phim

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,10 +3,10 @@ import Header from "./Header";
 import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
-    <div className="container">
+    <>
       <Header></Header> 
       <Outlet></Outlet>
-    </div>
+    </>
   );
 };
 export default Main;
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,45 @@
 import React from "react";
+import useSWR from "swr";
+import { fetcher } from "../config";
+import MovieCard from "../components/movie/MovieCard";
 
 const MoviePage = () => {
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=685814648889cc009fd6e5dd5ba7986b`,
+    fetcher
+  );
+  const movies = data?.results || [];
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus,
-      quos facilis maxime magni optio? Culpa laborum quasi, numquam, quos
-      dolorum aperiam dolorem nihil fugit ratione officiis hic cupiditate
-      quibusdam?
+    <div className="py-5">
+      <div className="flex mb-10">
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full p-4 bg-slate-800 text-white rounded-bl-xl rounded-tl-xl outline-none"
+            placeholder="Type here to search ...."
+          />
+        </div>
+        <button className="bg-primary p-3 rounded-tr-xl rounded-br-xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#fff"
+            class="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length &&
+          movies.map((item) => <MovieCard key={item.id} item={item} />)}
+      </div>
     </div>
   );
 };
